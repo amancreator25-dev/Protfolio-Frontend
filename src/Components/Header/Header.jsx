a9ab9e2b4631d198844d7cc81e27d89fa65816e7
Aman <amancreator25@gmail.com>
2026-07-20 23:20:55 +0530

    feat: Refactor Header component for improved user experience and dynamic navigation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import LogoImage from '../../assets/logO.png'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import LogoImage from "../../assets/logO.png";
 
 // Redux
-import { useSelector, useDispatch } from 'react-redux'
-import { logout } from '../../authSlice/authSlice.js'
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../authSlice/authSlice.js";
 
 export default function Header() {
     const dispatch = useDispatch();
-    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+    const { status, userData } = useSelector((state) => state.auth);
 
     return (
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-purple-500 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    
+
                     {/* Logo */}
                     <Link to="/" className="flex ml-[-200px] items-center">
                         <img
@@ -24,9 +25,10 @@ export default function Header() {
                         />
                     </Link>
 
-                    {/* Login / Signup OR Logout */}
+                    {/* Login / Register OR User */}
                     <div className="flex items-center ml-[400px] mr-[-200px] lg:order-2">
-                        {!isLoggedIn ? (
+
+                        {!status ? (
                             <>
                                 <NavLink
                                     to="/User/Login"
@@ -37,79 +39,161 @@ export default function Header() {
 
                                 <NavLink
                                     to="/User/Register"
-                                    className="text-white bg-purple-500 hover:bg-purple-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                                    className="text-white bg-purple-500 hover:bg-purple-700 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
                                 >
-                                    Get started
+                                    Get Started
                                 </NavLink>
                             </>
                         ) : (
-                            <button
-                                onClick={() => dispatch(logout())}
-                                className="text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
-                            >
-                                Logout
-                            </button>
+                            <div className="flex items-center gap-4">
+
+                                {/* Avatar */}
+                                <div className="w-10 h-10 rounded-full bg-purple-600 text-white flex items-center justify-center font-bold">
+                                    {userData?.fullname?.charAt(0).toUpperCase()}
+                                </div>
+
+                                {/* User Info */}
+                                <div className="hidden lg:block">
+                                    <p className="text-sm font-semibold">
+                                        {userData?.fullname}
+                                    </p>
+
+                                    <p className="text-xs text-gray-500">
+                                        @{userData?.username}
+                                    </p>
+                                </div>
+
+                                {/* Logout */}
+                                <button
+                                    onClick={() => dispatch(logout())}
+                                    className="text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-4 py-2"
+                                >
+                                    Logout
+                                </button>
+                            </div>
                         )}
+
                     </div>
 
-                    {/* Navigation Links */}
+                    {/* Navigation */}
                     <div
                         className="hidden ml-[450px] items-center w-full lg:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:items-center lg:space-x-15 lg:mt-0 lg:justify-center">
+
                             <li>
                                 <NavLink
                                     to="/"
                                     className={({ isActive }) =>
-                                        `block py-2 duration-200 ${isActive ? "text-purple-700" : "text-gray-700"} hover:text-purple-700`
+                                        `block py-2 duration-200 ${
+                                            isActive
+                                                ? "text-purple-700"
+                                                : "text-gray-700"
+                                        } hover:text-purple-700`
                                     }
                                 >
                                     Home
                                 </NavLink>
                             </li>
-                            <li>
-                                <NavLink
-                                    to="/contact"
-                                    className={({ isActive }) =>
-                                        `block py-2 duration-200 ${isActive ? "text-purple-700" : "text-gray-700"} hover:text-purple-700`
-                                    }
-                                >
-                                    Contact
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/github"
-                                    className={({ isActive }) =>
-                                        `block py-2 duration-200 ${isActive ? "text-purple-700" : "text-gray-700"} hover:text-purple-700`
-                                    }
-                                >
-                                    Github
-                                </NavLink>
-                            </li>
+
                             <li>
                                 <NavLink
                                     to="/Blogs"
                                     className={({ isActive }) =>
-                                        `block py-2 duration-200 ${isActive ? "text-purple-700" : "text-gray-700"} hover:text-purple-700`
+                                        `block py-2 duration-200 ${
+                                            isActive
+                                                ? "text-purple-700"
+                                                : "text-gray-700"
+                                        } hover:text-purple-700`
                                     }
                                 >
                                     Blogs
                                 </NavLink>
                             </li>
+
+                            {status && (
+                                <>
+                                    <li>
+                                        <NavLink
+                                            to="/create-blog"
+                                            className={({ isActive }) =>
+                                                `block py-2 duration-200 ${
+                                                    isActive
+                                                        ? "text-purple-700"
+                                                        : "text-gray-700"
+                                                } hover:text-purple-700`
+                                            }
+                                        >
+                                            Create Blog
+                                        </NavLink>
+                                    </li>
+
+                                    <li>
+                                        <NavLink
+                                            to="/my-blogs"
+                                            className={({ isActive }) =>
+                                                `block py-2 duration-200 ${
+                                                    isActive
+                                                        ? "text-purple-700"
+                                                        : "text-gray-700"
+                                                } hover:text-purple-700`
+                                            }
+                                        >
+                                            My Blogs
+                                        </NavLink>
+                                    </li>
+                                </>
+                            )}
+
+                            <li>
+                                <NavLink
+                                    to="/contact"
+                                    className={({ isActive }) =>
+                                        `block py-2 duration-200 ${
+                                            isActive
+                                                ? "text-purple-700"
+                                                : "text-gray-700"
+                                        } hover:text-purple-700`
+                                    }
+                                >
+                                    Contact
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink
+                                    to="/github"
+                                    className={({ isActive }) =>
+                                        `block py-2 duration-200 ${
+                                            isActive
+                                                ? "text-purple-700"
+                                                : "text-gray-700"
+                                        } hover:text-purple-700`
+                                    }
+                                >
+                                    Github
+                                </NavLink>
+                            </li>
+
                             <li>
                                 <NavLink
                                     to="/about"
                                     className={({ isActive }) =>
-                                        `block py-2 duration-200 ${isActive ? "text-purple-700" : "text-gray-700"} hover:text-purple-700`
+                                        `block py-2 duration-200 ${
+                                            isActive
+                                                ? "text-purple-700"
+                                                : "text-gray-700"
+                                        } hover:text-purple-700`
                                     }
                                 >
                                     About
                                 </NavLink>
                             </li>
+
                         </ul>
                     </div>
+
                 </div>
             </nav>
         </header>
